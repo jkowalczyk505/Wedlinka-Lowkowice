be81a0d54d69c2de66374f27e6aa345d00d7ce75
Emilia <afra57@onet.pl>
2025-07-01 00:23:01 +0200

    port server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
-require("dotenv").config(); // Jeśli używasz pliku .env
+require("dotenv").config();
 
-app.use(express.json()); // Parsowanie JSON
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend działa!");
 });
