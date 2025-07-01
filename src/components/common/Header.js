96f5a7417d5ecd2aea94b7ba0a5c26aa5c3d75aa
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-07-01 20:18:20 +0200

    loga i zmienne

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { IoCartOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+
 function Header() {
     return (
-        <p>Header</p>
+        <header className="header">
+            <div className="header-inner">
+                <div className="logo-wrapper">
+                    <img src={logo} alt="logo"></img>
+                </div>
+
+                <nav id="main-nav">
+                    <ul>
+                        <li><a href="#">Kiełbasy</a></li>
+                        <li><a href="#">Wędliny</a></li>
+                        <li><a href="#">Wyroby podrobowe</a></li>
+                        <li><a href="#">Nasze paczki</a></li>
+                        <li><a href="#">Kontakt</a></li>
+                    </ul>
+                </nav>
+
+                <div className="account-icons"></div>
+
+            </div>
+        </header>
     );
 }
 
