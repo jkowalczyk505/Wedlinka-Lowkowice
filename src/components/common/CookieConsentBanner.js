cb7dbead01afa592307cb5f165d6d2bd8ca05fa9
Emilia <afra57@onet.pl>
2025-07-02 22:13:32 +0200

    cookies baner

---
+++ src/components/common/CookieConsentBanner.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { MdOutlineCookie } from "react-icons/md";
+
+function CookieConsentBanner() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const consent = localStorage.getItem("cookie_consent");
+    if (!consent) setVisible(true);
+  }, []);
+
+  const handleAccept = () => {
+    localStorage.setItem("cookie_consent", "true");
+    setVisible(false);
+  };
+
+  return (
+    visible && (
+      <div className="cookie-banner">
+        <div className="cookie-icon">
+          <MdOutlineCookie size={40} />
+        </div>
+        <div className="cookie-text">
+          <h4>Ta strona używa ciasteczek</h4>
+          <p>
+            Używamy plików cookies niezbędnych do działania strony, logowania
+            oraz koszyka. Kontynuując, wyrażasz na to zgodę. Szczegóły
+            znajdziesz w naszej{" "}
+            <a href="/files/Polityka_Prywatnosci.pdf" download>
+              polityce prywatności
+            </a>
+          </p>
+        </div>
+        <button onClick={handleAccept} className="cookie-accept-btn">
+          Akceptuję
+        </button>
+      </div>
+    )
+  );
+}
+
+export default CookieConsentBanner;
