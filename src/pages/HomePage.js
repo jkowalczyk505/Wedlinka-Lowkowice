67c76738e6371596bf20689c268bd2a2650c6e92
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-07-05 00:54:21 +0200

    kategorie niedokonczone

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
 import Banner from "../components/common/Banner";
 import bannerImage from "../assets/home-banner.jpg";
 
+import CategoryTile from "../components/common/CategoryTile";
+import kielbasyImg from "../assets/kielbasy.jpg";
+import wedlinyImg from "../assets/wedliny.jpg";
+import podroboweImg from "../assets/podrobowe.jpg";
+import paczkiImg from "../assets/paczki.jpg";
+
 function HomePage() {
     return (
         <main className="page">
@@ -10,7 +16,18 @@ function HomePage() {
                 title="Wędliny i kiełbasy z tradycją"
                 subtitle="Wędliny, kiełbasy i wyroby podrobowe przygotowywane według tradycyjnych receptur, z najlepszych składników i bez zbędnych dodatków. Kupuj bezpośrednio od producenta."
             />
-            <section className=""></section>
+            <section className="products-section creamy-section">
+                <div className="products-info">
+                    <h2>Co znajdziesz w naszej ofercie?</h2>
+                    <p>Sprawdź, co oferujemy - wybierz interesującą Cię grupę.</p>
+                </div>
+                <div className="category-grid">
+                    <CategoryTile title="Kiełbasy" image={kielbasyImg} to="/kielbasy" />
+                    <CategoryTile title="Wędliny" image={wedlinyImg} to="/wedliny" />
+                    <CategoryTile title="Wyroby podrobowe" image={podroboweImg} to="/wyroby-podrobowe" />
+                    <CategoryTile title="Paczki" image={paczkiImg} to="/nasze-paczki" />
+                </div>
+            </section>
         </main>
     );
 }
