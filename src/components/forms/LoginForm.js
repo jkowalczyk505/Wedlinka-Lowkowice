efca88150592dce9400a7ae97acd87dfcc669ed3
Emilia <afra57@onet.pl>
2025-07-06 10:37:06 +0200

    remember me

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -23,7 +23,7 @@ function LoginForm() {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ email, password, remember }),
           credentials: "include",
         }
       );
