463b84a2574f28f91143dbeec1e37f1a230d834a
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-07-16 01:09:15 +0200

    admin dashboard

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -9,14 +9,11 @@ import {
 
 export default function AdminPage() {
   return (
-    <div className="admin-page-container white-section">
+    <div className="admin-page-container">
       {/* ── Sidebar ─────────────────────────────────── */}
       <aside className="admin-sidebar">
         <h2>Panel&nbsp;Admina</h2>
         <nav>
-          <NavLink to="/admin" end className={({isActive})=>isActive?"nav-link active":"nav-link"}>
-            <Home size={24}/>  Dashboard
-          </NavLink>
           <NavLink to="/admin/produkty"    className={({isActive})=>isActive?"nav-link active":"nav-link"}>
             <Package size={24}/> Produkty
           </NavLink>
